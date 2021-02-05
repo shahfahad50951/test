80a2e31b334fcc9dbaecc5edd3b1b82578a1fbf2
Shah Fahad <shahfahad.50951@gmail.com>
2021-02-05 09:31:52 +0530

    made a program to add 2 numbers

--- newfile.c
+++ newfile.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+int add(int , int);
+
 int main()
 {
+	int a = 0, b = 0;
 	printf("This is first line of code in git repository\n");
 	printf("This line of code is from git web\n");
+	printf("Enter 2 numbers to be added: ");
+	scanf("%d%d",&a,&b);
+	printf("The sum of 2 numbers is : %d\n", add(a, b));
 	return 0;
+}
+int add(int a , int b)
+{
+	return a+b;
 }
